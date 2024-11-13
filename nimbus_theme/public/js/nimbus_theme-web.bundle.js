bbb2d078c28be65d389c0c2cfcf47f63cd63ee9d
Panayiotis Halouvas <phalouvas@gmail.com>
2024-11-13 06:06:41 +0000

    feat: Update particle loading to check for element existence and rename section class

--- nimbus_theme/public/js/nimbus_theme-web.bundle.js
+++ nimbus_theme/public/js/nimbus_theme-web.bundle.js
@@ -1,7 +1,8 @@
 import "./gdpr-cookie.js";
 import "./particles/particles.js";
 
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', '/assets/nimbus_theme/js/particles/particles.json', function() {
-    console.log('callback - particles.js config loaded');
+if (document.getElementById('nimbus-particles')) {
+  particlesJS.load('nimbus-particles', '/assets/nimbus_theme/js/particles/particles.json', function() {
+    //console.log('callback - particles.js config loaded');
   });
+}
