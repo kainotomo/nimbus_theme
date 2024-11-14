b409367df0c6bac665d3a0e6029030d5056e3ba4
Panayiotis Halouvas <phalouvas@gmail.com>
2024-11-14 07:11:52 +0000

    feat: Add sticky navbar functionality and styles

--- nimbus_theme/public/js/nimbus_theme-web.bundle.js
+++ nimbus_theme/public/js/nimbus_theme-web.bundle.js
@@ -2,6 +2,7 @@ import "./gdpr-cookie.js";
 import "./particles/particles.js";
 import "./animations.js";
 import "./back-to-top.js";
+import "./navbar.js";
 
 if (document.getElementById('nimbus-particles')) {
   particlesJS.load('nimbus-particles', '/assets/nimbus_theme/js/particles/particles.json', function() {
