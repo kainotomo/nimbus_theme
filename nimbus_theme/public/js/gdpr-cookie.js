7778aa3f317a6e1fa90bf0a7029a0a618d5e3267
Panayiotis Halouvas <phalouvas@gmail.com>
2025-04-09 04:58:25 +0000

    Added Google Analytics consent

--- nimbus_theme/public/js/gdpr-cookie.js
+++ nimbus_theme/public/js/gdpr-cookie.js
@@ -1,14 +1,43 @@
+// Set up Google Consent Mode v2
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+
+// Default consent settings - denied by default for EEA region
+gtag('consent', 'default', {
+    'ad_storage': 'denied',            // Advertising cookies
+    'analytics_storage': 'denied',      // Analytics cookies
+    'ad_user_data': 'denied',          // User data for advertising
+    'ad_personalization': 'denied',     // Personalized advertising
+    'region': ['EEA']                   // Apply these settings to EEA users
+});
+
 $(document).ready(function () {
     // Check if the 'gdprAccepted' cookie is not set
     if (!getCookie('gdprAccepted')) {
         // Show the GDPR banner
         $('#gdpr-banner').show();
+    } else {
+        // If user already accepted, update consent mode
+        gtag('consent', 'update', {
+            'ad_storage': 'granted',
+            'analytics_storage': 'granted',
+            'ad_user_data': 'granted',
+            'ad_personalization': 'granted'
+        });
     }
 
     // When the accept button is clicked
     $('#accept-gdpr').click(function () {
         // Set the 'gdprAccepted' cookie to 'true'
         setCookie('gdprAccepted', 'true', 365);
+
+        // Update Google Consent Mode when user accepts
+        gtag('consent', 'update', {
+            'ad_storage': 'granted',
+            'analytics_storage': 'granted',
+            'ad_user_data': 'granted',
+            'ad_personalization': 'granted'
+        });
 
         // Hide the GDPR banner
         $('#gdpr-banner').hide();
